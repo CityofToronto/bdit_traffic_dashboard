bc63aa30928bd9712f9af220ed08c5f23fa8d60b
Raphael Dumas <radumas@users.noreply.github.com>
2019-09-23 15:33:11 -0400

    Patch
    remove import of dash_bootstrap_components because not being used yet.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from datetime import datetime, date
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 import pandas as pd
 import pandas.io.sql as pandasql
 from numpy import nan
