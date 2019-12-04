51552cb27b77fc713542d8d8426533f5a62d12b2
chmnata <chmnata@gmail.com>
2019-12-04 11:45:39 -0500

    changed to gardiner query

--- sql/dash.sql
+++ sql/dash.sql
@@ -1,23 +1,36 @@
 --SQL we used to read data from our rdb to dash in app.py
 
 --DATA
-SELECT street, case when direction = 'EB' then 'Eastbound'
-                        when direction = 'WB' then 'Westbound'
-                        when direction = 'NB' then 'Northbound'
-                        when direction ='SB' then 'Southbound' end as direction, 
-                        date, day_type, category, period, tt, most_recent, week_number, month_number from data_analysis.richmond_dash_daily
-
+ SELECT d.street as street_short, case when direction = 'EB' then 'Eastbound'
+                            when direction = 'WB' then 'Westbound'
+                            when direction = 'NB' then 'Northbound'
+                            when direction ='SB' then 'Southbound' 
+                            else direction end as direction, COALESCE(name1, d.street || ': ' || d.start_crossstreet || ' and ' || d.end_crossstreet) as street, 
+                            date, day_type, category, period, round(tt/60,2) as tt, most_recent, week_number, month_number 
+                            FROM data_analysis.gardiner_dash_daily_mat d		
+                            left join (SELECT DISTINCT ON (d1.street, d1.start_cross, d1.end_cross) d1.street, d1.start_cross, d1.end_cross, 
+                                        d1.street || ': ' || d1.start_cross || ' and ' || d1.end_cross as name1
+                                        FROM data_analysis.gardiner_segments d1, data_analysis.gardiner_segments d2 
+                                        WHERE d1.street = d2.street and d1.start_cross = d2.end_cross and d1.direction in ('NB', 'EB')
+                                        ORDER BY d1.street) n	
+                            ON n.street = d.street AND ( (n.start_cross = d.start_crossstreet AND n.end_cross = d.end_crossstreet) OR (n.start_cross = d.end_crossstreet AND n.end_cross = d.start_crossstreet)) 									
+                            order by d.street, d.direction, start_crossstreet 
 --BASELINE
-SELECT street, street_suffix, direction, from_intersection, to_intersection, 
-                             day_type, period, to_char(lower(period_range::TIMERANGE), 'FMHH AM')||' to '||to_char(upper(period_range::TIMERANGE), 'FMHH AM') as period_range , tt
-                             FROM data_analysis.richmond_dash_baseline
-                             order by richmond_dash_baseline.period_range
+SELECT COALESCE(name1, d.street || ': ' || d.from_intersection || ' and ' || d.to_intersection) as street, d.street as street_short,
+                                d.direction, from_intersection, to_intersection,day_type, period, to_char(lower(period_range::TIMERANGE), 'FMHH AM')||' to '||to_char(upper(period_range::TIMERANGE), 'FMHH AM') as period_range , tt
+                                FROM data_analysis.gardiner_dash_baseline_mat d 
+                                left join (SELECT DISTINCT ON (d1.street, d1.start_cross, d1.end_cross) d1.street, d1.start_cross, d1.end_cross, 
+                                                            d1.street || ': ' || d1.start_cross || ' and ' || d1.end_cross as name1
+                                                            FROM data_analysis.gardiner_segments d1, data_analysis.gardiner_segments d2 
+                                                            WHERE d1.street = d2.street and d1.start_cross = d2.end_cross and d1.direction in ('NB', 'EB')
+                                                            ORDER BY d1.street) n																																   
+                                ON n.street = d.street AND ( (n.start_cross = d.from_intersection AND n.end_cross = d.to_intersection) OR (n.start_cross = d.to_intersection AND n.end_cross = d.from_intersection)) 
 
 --HOLIDAY
-SELECT dt FROM ref.holiday WHERE dt > '2019-07-02'
+SELECT dt FROM ref.holiday WHERE dt >= '2019-08-01'
 
 --WEEK
-SELECT * FROM data_analysis.richmond_closure_weeks order by week_number desc
+SELECT * FROM data_analysis.gardiner_weeks
 
 --MONTH
-SELECT * FROM data_analysis.richmond_closure_months order by month_number desc
+SELECT * FROM data_analysis.gardiner_months
--- sql/month_week.sql
+++ sql/month_week.sql
@@ -1,22 +1,23 @@
 --SQL for generating month number and month for aggregation with the daily view
 --used the dates from our data to make sure months are only created if there are data
 
-CREATE OR REPLACE VIEW data_analysis.richmond_closure_months AS
- SELECT a.month,
-    row_number() OVER () AS month_number
-   FROM ( SELECT DISTINCT ON ((date_trunc('month'::text, watermain_blip_30min.dt::timestamp with time zone)::date)) date_trunc('month'::text, watermain_blip_30min.dt::timestamp with time zone)::date AS month
-           FROM data_analysis.watermain_blip_30min
-          WHERE watermain_blip_30min.dt >= '2019-07-22'::date) a
-  ORDER BY a.month;
+CREATE OR REPLACE VIEW data_analysis.gardiner_weeks AS
+ SELECT a.dt::date AS week,
+    row_number() OVER () AS week_number
+   FROM ( SELECT generate_series('2019-08-01 00:00:00'::timestamp without time zone, '2019-12-01 00:00:00'::timestamp without time zone, '7 days'::interval) AS dt) a
+  ORDER BY (row_number() OVER ()) DESC;
+
+GRANT SELECT ON TABLE data_analysis.gardiner_weeks TO bdit_humans;
+GRANT SELECT ON TABLE data_analysis.gardiner_weeks TO heroku_bot;
 
 --SQL for generating week number and week for aggregation with the daily view
 --used the dates from our data to make sure months are only created if there are data
-CREATE OR REPLACE VIEW data_analysis.richmond_closure_weeks AS
- SELECT a.week,
-    row_number() OVER () AS week_number
-   FROM ( SELECT DISTINCT ON ((date_trunc('week'::text, watermain_blip_30min.dt::timestamp with time zone)::date)) date_trunc('week'::text, watermain_blip_30min.dt::timestamp with time zone)::date AS week
-           FROM data_analysis.watermain_blip_30min
-          WHERE watermain_blip_30min.dt >= '2019-07-22'::date) a
-  ORDER BY a.week;
+CREATE OR REPLACE VIEW data_analysis.gardiner_months AS
+ SELECT a.dt::date AS month,
+    row_number() OVER () AS month_number
+   FROM ( SELECT generate_series('2019-08-01 00:00:00'::timestamp without time zone, '2019-12-01 00:00:00'::timestamp without time zone, '1 mon'::interval) AS dt) a
+  ORDER BY (row_number() OVER ()) DESC;
 
+GRANT SELECT ON TABLE data_analysis.gardiner_months TO bdit_humans;
+GRANT SELECT ON TABLE data_analysis.gardiner_weeks TO heroku_bot;
 
