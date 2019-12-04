6bc6a86eb04833f4a49247992a566eac844f56e4
chmnata <chmnata@gmail.com>
2019-12-04 14:19:38 -0500

    change month/week sql to generate day up to yesterday

--- sql/month_week.sql
+++ sql/month_week.sql
@@ -4,7 +4,7 @@
 CREATE OR REPLACE VIEW data_analysis.gardiner_weeks AS
  SELECT a.dt::date AS week,
     row_number() OVER () AS week_number
-   FROM ( SELECT generate_series('2019-08-01 00:00:00'::timestamp without time zone, '2019-12-01 00:00:00'::timestamp without time zone, '7 days'::interval) AS dt) a
+   FROM ( SELECT generate_series('2019-08-01 00:00:00'::timestamp without time zone, (now()-interval'1 day')::timestamp without time zone, '7 days'::interval) AS dt) a
   ORDER BY (row_number() OVER ()) DESC;
 
 GRANT SELECT ON TABLE data_analysis.gardiner_weeks TO bdit_humans;
@@ -13,10 +13,10 @@ GRANT SELECT ON TABLE data_analysis.gardiner_weeks TO heroku_bot;
 --SQL for generating week number and week for aggregation with the daily view
 --used the dates from our data to make sure months are only created if there are data
 CREATE OR REPLACE VIEW data_analysis.gardiner_months AS
- SELECT a.dt::date AS month,
+SELECT a.dt::date AS month,
     row_number() OVER () AS month_number
-   FROM ( SELECT generate_series('2019-08-01 00:00:00'::timestamp without time zone, '2019-12-01 00:00:00'::timestamp without time zone, '1 mon'::interval) AS dt) a
-  ORDER BY (row_number() OVER ()) DESC;
+   FROM ( SELECT generate_series('2019-08-01 00:00:00'::timestamp without time zone, (now()-interval'1 day')::timestamp without time zone, '1 mon'::interval) AS dt) a
+  ORDER BY (row_number() OVER ()) DESC;		
 
 GRANT SELECT ON TABLE data_analysis.gardiner_months TO bdit_humans;
 GRANT SELECT ON TABLE data_analysis.gardiner_weeks TO heroku_bot;
