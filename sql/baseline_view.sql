009fb0b34c5be5313fe434e610f316a0e83a4924
chmnata <chmnata@gmail.com>
2019-12-13 16:11:22 -0500

    update basleine and daily view

--- sql/baseline_view.sql
+++ sql/baseline_view.sql
@@ -1,39 +1,27 @@
 -- a view in our database that aggregates our baseline data
-CREATE OR REPLACE VIEW data_analysis.richmond_dash_baseline AS
- WITH seg_num AS (
-         SELECT watermain_blip_daily.street,
-            watermain_blip_daily.direction,
-            count(DISTINCT watermain_blip_daily.start_crossstreet) AS number_seg
-           FROM data_analysis.watermain_blip_daily
-          GROUP BY watermain_blip_daily.street, watermain_blip_daily.direction
-        ), tempa AS (
-         SELECT a.street, from_intersection, to_intersection, street_suffix,
-                CASE
-                    WHEN a.direction = 'NB'::text THEN 'Northbound'::text
-                    WHEN a.direction = 'EB'::text THEN 'Eastbound'::text
-                    WHEN a.direction = 'WB'::text THEN 'Westbound'::text
-                    WHEN a.direction = 'SB'::text THEN 'Southbound'::text
-                    ELSE NULL::text
-                END AS direction,
-            a.study_period,
-            a.day_type,
-            a.dt,
-            a.period,
-            richmond_periods.time_range AS period_range,
-            sum(a.tt) AS tt
-           FROM data_analysis.watermain_blip_daily a
-             JOIN data_analysis.richmond_segment_lookup using (street, direction)
-             JOIN data_analysis.richmond_periods USING (period, day_type)
-             JOIN seg_num b USING (street, direction)
-          WHERE a.study_period = 'Baseline'::text
-          GROUP BY a.street, a.direction, a.study_period, a.day_type, a.period, a.dt, richmond_periods.time_range, b.number_seg, from_intersection, to_intersection, street_suffix
-         HAVING count(1) = b.number_seg
-        )
- SELECT tempa.street,street_suffix, tempa.direction,from_intersection,to_intersection,
-    tempa.day_type,
-    tempa.period,
-    tempa.period_range,
-    round(avg(tempa.tt) / 60::numeric, 1) AS tt
-   FROM tempa
-  GROUP BY tempa.street, tempa.direction, tempa.from_intersection, tempa.to_intersection, tempa.day_type, tempa.period, tempa.period_range, street_suffix
-  order by street
+CREATE OR REPLACE VIEW data_analysis.gardiner_dash_baseline AS
+ SELECT gardiner_blip_daily.street,
+        CASE
+            WHEN gardiner_blip_daily.direction::text = 'NB'::text THEN 'Northbound'::text
+            WHEN gardiner_blip_daily.direction::text = 'EB'::text THEN 'Eastbound'::text
+            WHEN gardiner_blip_daily.direction::text = 'WB'::text THEN 'Westbound'::text
+            WHEN gardiner_blip_daily.direction::text = 'SB'::text THEN 'Southbound'::text
+            ELSE NULL::text
+        END AS direction,
+    gardiner_blip_daily.start_crossstreet AS from_intersection,
+    gardiner_blip_daily.end_crossstreet AS to_intersection,
+    gardiner_blip_daily.day_type,
+    gardiner_blip_daily.period,
+    gardiner_periods.time_range AS period_range,
+    round(avg(gardiner_blip_daily.tt) / 60::numeric, 1) AS tt
+   FROM data_analysis.gardiner_blip_daily
+     JOIN data_analysis.gardiner_periods USING (period, day_type)
+  WHERE gardiner_blip_daily.study_period = 'Baseline'::text
+  GROUP BY gardiner_blip_daily.street, gardiner_blip_daily.direction, gardiner_blip_daily.start_crossstreet, gardiner_blip_daily.end_crossstreet, gardiner_blip_daily.day_type, gardiner_blip_daily.period, gardiner_periods.time_range;
+
+ALTER TABLE data_analysis.gardiner_dash_baseline
+    OWNER TO natalie;
+
+GRANT SELECT ON TABLE data_analysis.gardiner_dash_baseline TO bdit_humans;
+GRANT ALL ON TABLE data_analysis.gardiner_dash_baseline TO natalie;
+GRANT SELECT ON TABLE data_analysis.gardiner_dash_baseline TO heroku_bot;
--- sql/daily_view.sql
+++ sql/daily_view.sql
@@ -1,40 +1,28 @@
 -- a view in our database that aggregates our daily data
 
-CREATE OR REPLACE VIEW data_analysis.richmond_dash_daily AS
- WITH seg_num AS (
-         SELECT watermain_blip_daily.street,
-            watermain_blip_daily.direction,
-            count(DISTINCT watermain_blip_daily.start_crossstreet) AS number_seg
-           FROM data_analysis.watermain_blip_daily
-          GROUP BY watermain_blip_daily.street, watermain_blip_daily.direction
-        ), tempa AS (
-         SELECT watermain_blip_daily.street,
-            watermain_blip_daily.direction,
-            watermain_blip_daily.study_period AS category,
-            watermain_blip_daily.dt,
-            watermain_blip_daily.day_type,
-            watermain_blip_daily.period,
-            sum(watermain_blip_daily.tt) / 60::numeric AS tt,
-            count(1) AS count,
-            a.number_seg
-           FROM data_analysis.watermain_blip_daily
-             JOIN seg_num a USING (street, direction)
-          GROUP BY watermain_blip_daily.street, watermain_blip_daily.direction, watermain_blip_daily.study_period, watermain_blip_daily.dt, watermain_blip_daily.day_type, watermain_blip_daily.period, a.number_seg
-         HAVING count(1) = a.number_seg
-        )
- SELECT tempa.street,
-    tempa.direction,
-    tempa.dt AS date,
-    tempa.day_type,
-    tempa.category,
-    tempa.period,
-    round(tempa.tt, 1) AS tt,
+CREATE OR REPLACE VIEW data_analysis.gardiner_dash_daily AS
+ SELECT gardiner_blip_daily.street,
+    gardiner_blip_daily.direction,
+    gardiner_blip_daily.start_crossstreet,
+    gardiner_blip_daily.end_crossstreet,
+    gardiner_blip_daily.dt AS date,
+    gardiner_blip_daily.day_type,
+    gardiner_blip_daily.study_period AS category,
+    gardiner_blip_daily.period,
+    gardiner_blip_daily.tt,
         CASE
-            WHEN tempa.dt = first_value(tempa.dt) OVER (PARTITION BY tempa.direction, tempa.day_type, tempa.period ORDER BY tempa.dt DESC) THEN 1
+            WHEN gardiner_blip_daily.dt = first_value(gardiner_blip_daily.dt) OVER (PARTITION BY gardiner_blip_daily.direction, gardiner_blip_daily.day_type, gardiner_blip_daily.period ORDER BY gardiner_blip_daily.dt DESC) THEN 1
             ELSE 0
         END AS most_recent,
     weeks.week_number,
     months.month_number
-   FROM tempa
-     LEFT JOIN data_analysis.richmond_closure_weeks weeks ON tempa.dt >= weeks.week AND tempa.dt < (weeks.week + '7 days'::interval)
-     LEFT JOIN data_analysis.richmond_closure_months months ON tempa.dt >= months.month AND tempa.dt < (months.month + '1 mon'::interval);
+   FROM data_analysis.gardiner_blip_daily
+     LEFT JOIN data_analysis.gardiner_weeks weeks ON gardiner_blip_daily.dt >= weeks.week AND gardiner_blip_daily.dt < (weeks.week + '7 days'::interval)
+     LEFT JOIN data_analysis.gardiner_months months ON gardiner_blip_daily.dt >= months.month AND gardiner_blip_daily.dt < (months.month + '1 mon'::interval);
+
+ALTER TABLE data_analysis.gardiner_dash_daily
+    OWNER TO natalie;
+
+GRANT SELECT ON TABLE data_analysis.gardiner_dash_daily TO bdit_humans;
+GRANT ALL ON TABLE data_analysis.gardiner_dash_daily TO natalie;
+GRANT SELECT ON TABLE data_analysis.gardiner_dash_daily TO heroku_bot;
